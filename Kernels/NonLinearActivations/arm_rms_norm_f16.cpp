435e119e4781c4b968cc41573a5e27a281c4e6b7
Christophe Favergeon <Christophe.Favergeon@arm.com>
2024-04-09 14:19:13 +0200

    Use CMSIS-DSP CPP extension for some kernels.

--- Kernels/NonLinearActivations/arm_rms_norm_f16.cpp
+++ Kernels/NonLinearActivations/arm_rms_norm_f16.cpp
@@ -4,18 +4,25 @@
 #include "dsp/statistics_functions_f16.h"
 #include "dsp/basic_math_functions_f16.h"
 
+#include <dsppp/memory_pool.hpp>
+#include <dsppp/matrix.hpp>
+
+using namespace arm_cmsis_dsp;
+
 void arm_rms_norm_f16(float16_t* o, float16_t* x, float16_t* weight, int size) {
 
     // calculate sum of squares
-    float16_t ss = 0.0f16;
-    arm_power_f16(x, size, &ss);
+    VectorView<float16_t> xV(x,0,size);
+    VectorView<float16_t> oV(o,0,size);
+    VectorView<float16_t> weightV(weight,0,size);
+     
+    float16_t ss = dot(xV,xV);
     ss /= (_Float16)size;
     ss += 1e-5f16;
     ss = 1.0f16 / (_Float16)sqrtf((float)ss);
 
     // normalize and scale
-    arm_scale_f16(x,ss,o,size);
-    arm_mult_f16(weight,o,o,size);
-    
+
+    oV = weightV * (xV * ss);
 
 }
--- Kernels/NonLinearActivations/arm_softmax_f16.cpp
+++ Kernels/NonLinearActivations/arm_softmax_f16.cpp
@@ -4,6 +4,7 @@
 #include "dsp/statistics_functions_f16.h"
 #include "dsp/basic_math_functions_f16.h"
 
+
 void arm_softmax_f16(float16_t* x, int size) {
 
     // find max value (for numerical stability)
@@ -11,9 +12,13 @@ void arm_softmax_f16(float16_t* x, int size) {
     arm_max_no_idx_f16(x, size, &max_val);
 
     // exp and sum
+   
+    arm_offset_f16(x,-max_val,x,size);
+    arm_vexp_f16(x,x,size);
+
     float16_t sum = 0.0f16;
-    for (int i = 0; i < size; i++) {
-        x[i] = (_Float16)expf((float)((_Float16)x[i] - (_Float16)max_val));
+    for (int i = 0; i < size; i++) 
+    {
         sum += (_Float16)x[i];
     }
 
